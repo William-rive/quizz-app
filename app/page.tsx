50a36184325eba694e6279fc50489fc903cb0e5d
William-rive <william.rive1999@gmail.com>
2024-11-12 16:50:43 +0100

    Prototype de filtre pour les categories(appel)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 import { useEffect, useState } from 'react';
 import fetchDatabase from './lib/api';
+import getCategories from './lib/filter';
 
 export default function Page() {
   const [data, setData] = useState(null);
+  const [categories, setCategories] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,9 +18,30 @@ export default function Page() {
     }, 1000);
   }, []);
 
+  useEffect(() => {
+    const fetchCategories = async () => {
+      if (data) {
+        const categories = await getCategories();
+        setCategories(categories);
+      }
+    };
+
+    fetchCategories();
+  }, [data]);
+
   return (
     <div>
       {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
+      {categories ? (
+        <ul>
+          {categories.map(category => (
+            <li key={category.id}>{category.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading categories...</p>
+      )}
     </div>
   );
+
 }
