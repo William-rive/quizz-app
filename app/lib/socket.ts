f94b710e2fdeeb27a41ebefde3831595f08f6c9c
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2024-12-02 11:36:41 +0100

    Refactorisation de la gestion des sockets : remplacement de l'initialisation dynamique par une instance unique, amélioration de la persistance de la connexion et ajout de logs pour le débogage.

--- app/lib/socket.ts
+++ app/lib/socket.ts
@@ -1,31 +1,20 @@
 // app/lib/socket.ts
 'use client';
-
 import { io, Socket } from 'socket.io-client';
 import { getUserId } from './userId';
-
-interface Auth {
-  userId: string;
-}
 
 interface CustomSocket extends Socket {
   auth: Auth;
 }
 
-let socket: CustomSocket | null = null;
+const userId = getUserId();
 
-export const initializeSocket = (): CustomSocket => {
-  if (!socket) {
-    const userId = getUserId();
-    socket = io('http://localhost:3001', {
-      autoConnect: false,
-      auth: {
-        userId, // Envoyer l'ID utilisateur lors de la connexion
-      },
-    }) as CustomSocket;
-  }
-  return socket;
-};
+const socket = io('http://localhost:3001', {
+  autoConnect: false,
+  auth: {
+    userId, // Envoyer l'ID utilisateur lors de la connexion
+  },
+}) as CustomSocket;
 
 export default socket;
 export type { CustomSocket };
